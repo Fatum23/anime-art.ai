f737855929f8a05b4a4f419918be87844f68ac9f
Fatum23 <dmitrijnaumov342@gmail.com>
2024-08-20 22:18:47 +0300

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { Description } from './Description'
-import { Example } from './Example'
-import { NavBar } from './NavBar'
+import { Description } from "./Description";
+import { Example } from "./Example";
+import { NavBar } from "./NavBar";
+import { UploadPhoto } from "./UploadPhoto";
 
 export const App = () => {
-	return (
-		<div className='w-screen h-screen'>
-			<NavBar />
-			<div className='m-3 flex flex-col gap-3'>
-				<Description />
-				<Example />
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="w-screen h-screen">
+      <NavBar />
+      <div className="m-3 flex flex-col gap-3">
+        <Description />
+        <Example />
+        <UploadPhoto />
+      </div>
+    </div>
+  );
+};
--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,15 +1,15 @@
 export const NavBar = () => {
-	return (
-		<>
-			<div className='bg-violet-900 w-full h-16 fixed rounded-b-xl flex flex-row items-center justify-between px-4 gap-4'>
-				<h1>0 credits</h1>
-				<div className='flex flex-row gap-4'>
-					<button>Login</button>
-					<button>Sign Up</button>
-				</div>
-				{/* //TODO isLogined && Buy credits button */}
-			</div>
-			<div className='h-16'></div>
-		</>
-	)
-}
+  return (
+    <>
+      <div className="bg-violet-700 w-full h-16 fixed rounded-b-xl flex flex-row items-center justify-between px-3 gap-3">
+        <h1>0 credits</h1>
+        <div className="flex flex-row gap-3">
+          <button>Login</button>
+          <button>Sign Up</button>
+        </div>
+        {/* //TODO isLogined && Buy credits button */}
+      </div>
+      <div className="h-16"></div>
+    </>
+  );
+};
